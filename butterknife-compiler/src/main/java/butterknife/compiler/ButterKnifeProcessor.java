683f775728b1c8ecb1b7f4aed6cc371b6caf11c4
Felipe Lima <felipe.lima@gmail.com>
2016-10-26 13:59:02 -0700

    Undo some changes

--- butterknife-compiler/src/main/java/butterknife/compiler/ButterKnifeProcessor.java
+++ butterknife-compiler/src/main/java/butterknife/compiler/ButterKnifeProcessor.java
@@ -1311,9 +1311,6 @@ public final class ButterKnifeProcessor extends AbstractProcessor {
     private final Map<String, String> rClasses = new LinkedHashMap<>();
     private String currentPackageName;
 
-    RClassScanner() {
-    }
-
     @Override public void visitSelect(JCTree.JCFieldAccess jcFieldAccess) {
       Symbol symbol = jcFieldAccess.sym;
       if (symbol != null
